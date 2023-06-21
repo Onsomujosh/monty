c070cc3986365cf0111456ff4a917aec71ff2803
Onsomujosh <Joshuaonsomu@gmail.com>
2023-06-21 08:56:08 -0700

    push, pall

--- monty.h
+++ monty.h
@@ -1,5 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -38,7 +39,27 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void f_push(stack_t **head, unsigned int counter);
-void f_pall(stack_t **head, unsigned int counter)
+/**
+ * struct bus_s -variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+} bus_t;
+extern bus_t bus;
 
-#endif
+void f_push(stack_t **head, unsigned int counter);
+void f_pall(stack_t **head, unsigned int counter);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
+void free_stack(stack_t *head);
+
+#endif /*MONTY_H*/
--- pall.c
+++ pall.c
@@ -1,10 +1,12 @@
 #include "monty.h"
+
 /**
  * f_pall - prints all values of the stack
  * @head: stack head
  * @counter: number
  * Return: void
 */
+
 void f_pall(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
@@ -13,6 +15,7 @@ void f_pall(stack_t **head, unsigned int counter)
 	h = *head;
 	if (h == NULL)
 		return;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
